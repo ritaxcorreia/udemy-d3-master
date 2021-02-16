24cb92109c1a5312c6fbe75fd3aa30e6239ba2fe
Rita <rita@ritacorreia.co>
2021-02-16 12:53:23 +0000

    First data visualisation

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -1,5 +1,41 @@
 /*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    2.8 - Activity: Your first visualization!
-*/
+ *    main.js
+ *    Mastering Data Visualization with D3.js
+ *    2.8 - Activity: Your first visualization!
+ */
+
+// Fetches JSON data
+d3.json("data/buildings.json")
+	.then((data) => {
+		data.forEach((d) => {
+			d.height = Number(d.height);
+		});
+		console.log(data);
+
+		// Map chart area
+		const svg = d3
+			.select("#chart-area")
+			.append("svg")
+			.attr("width", 500)
+			.attr("height", 700);
+
+		// Generates data visualisation as a bar chart
+		const bars = svg.selectAll("rect").data(data);
+
+		bars.enter()
+			.append("rect")
+			.attr("x", (d, i) => i * 50 + 50)
+			.attr("y", 0)
+			.attr("height", (d) => d.height)
+			.attr("width", 40)
+			.attr("fill", (d) => {
+				if (d.name === "Burj Khalifa") {
+					return "red";
+				} else {
+					return "pink";
+				}
+			});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
