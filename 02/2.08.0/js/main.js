36c35bbf860765358095a5eec39cb4083e3186e9
Rita <rita@ritacorreia.co>
2021-02-16 17:01:11 +0000

    Tallest buildings data visualisation

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -16,18 +16,31 @@ d3.json("data/buildings.json")
 		const svg = d3
 			.select("#chart-area")
 			.append("svg")
-			.attr("width", 500)
-			.attr("height", 700);
+			.attr("width", 600)
+			.attr("height", 600);
+
+		const x = d3
+			.scaleBand()
+			.domain(data.map((d) => d.name))
+			// .domain([d3.min(data, (d) => d.name), d3.max(data, (d) => d.name)])
+			.range([0, 400])
+			.paddingInner(0.3)
+			.paddingOuter(0.1);
+
+		const y = d3
+			.scaleLinear()
+			.domain([0, d3.max(data, (d) => d.height)])
+			.range([0, 400]);
 
 		// Generates data visualisation as a bar chart
 		const bars = svg.selectAll("rect").data(data);
 
 		bars.enter()
 			.append("rect")
-			.attr("x", (d, i) => i * 50 + 50)
+			.attr("x", (d) => x(d.name))
 			.attr("y", 0)
-			.attr("height", (d) => d.height)
-			.attr("width", 40)
+			.attr("height", (d) => y(d.height))
+			.attr("width", x.bandwidth)
 			.attr("fill", (d) => {
 				if (d.name === "Burj Khalifa") {
 					return "red";
