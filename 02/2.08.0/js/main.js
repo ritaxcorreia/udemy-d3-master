cbd42ab336a77a5c04d84eb4587ad2ee4c311c80
Rita <rita@ritacorreia.co>
2021-02-17 17:01:56 +0000

    finish tallest buildings visualisation

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -13,9 +13,9 @@ d3.json("data/buildings.json")
 		console.log(data);
 
 		// Defines margins for svg group
-		const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 };
+		const MARGIN = { LEFT: 130, RIGHT: 10, TOP: 10, BOTTOM: 130 };
 		const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
-		const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
+		const HEIGHT = 600 - MARGIN.TOP - MARGIN.BOTTOM;
 
 		// Map chart area
 		const svg = d3
@@ -29,6 +29,25 @@ d3.json("data/buildings.json")
 			.append("g")
 			.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
+		// Label on X axis
+		g.append("text")
+			.attr("class", "x axis-label")
+			.attr("x", WIDTH / 2)
+			.attr("y", HEIGHT + 110)
+			.attr("font-size", "16px")
+			.attr("text-anchor", "middle")
+			.text("The world's tallest buildings");
+
+		// Label on Y axis
+		g.append("text")
+			.attr("class", "y axis-label")
+			.attr("x", -(HEIGHT / 2))
+			.attr("y", "-60")
+			.attr("font-size", "16px")
+			.attr("transform", "rotate(-90)")
+			.attr("text-anchor", "middle")
+			.text("Height (m)");
+
 		const x = d3
 			.scaleBand()
 			.domain(data.map((d) => d.name))
@@ -39,12 +58,26 @@ d3.json("data/buildings.json")
 		const y = d3
 			.scaleLinear()
 			.domain([0, d3.max(data, (d) => d.height)])
-			.range([0, HEIGHT]);
+			.range([HEIGHT, 0]);
 
 		// Defines the axes
 		const xAxisCall = d3.axisBottom(x);
+		g.append("g")
+			.attr("class", "x axis")
+			.attr("transform", `translate(0, ${HEIGHT})`)
+			.call(xAxisCall)
+			.selectAll("text")
+			.attr("y", "10")
+			.attr("x", "-5")
+			.attr("text-anchor", "end")
+			.attr("transform", "rotate(-40)");
 
-		const yAxisCall = d3.axisLeft(y);
+		const yAxisCall = d3
+			.axisLeft(y)
+			.ticks(3)
+			.tickFormat((d) => d + "m");
+
+		g.append("g").attr("class", "y axis").call(yAxisCall);
 
 		// Generates data visualisation as a bar chart
 		const bars = g.selectAll("rect").data(data);
@@ -52,8 +85,8 @@ d3.json("data/buildings.json")
 		bars.enter()
 			.append("rect")
 			.attr("x", (d) => x(d.name))
-			.attr("y", 0)
-			.attr("height", (d) => y(d.height))
+			.attr("y", (d) => y(d.height))
+			.attr("height", (d) => HEIGHT - y(d.height))
 			.attr("width", x.bandwidth)
 			.attr("fill", (d) => {
 				if (d.name === "Burj Khalifa") {
